a31c7f437df2075e68017267a05ec397efaff4a9
chmohcen <chaouimohcen@gmail.com>
2026-07-26 19:16:50 +0100

    feat(admin): enable searching users by name

--- chat_backend/main/admin.py
+++ chat_backend/main/admin.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 from .models import User, Message
 
 
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+
+
 class MessageAdmin(admin.ModelAdmin):
     list_filter = ('sender', 'receiver')
 
 
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Message, MessageAdmin)
